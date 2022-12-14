c5c6fe09e6719445a9643abdcf483c5b563f39ae
VKj <kjartanosom@gmail.com>
2022-12-14 23:02:51 +0100

    1.0

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import VL from "./VL"
+import "./generalstyles.css"
+import "./home.css"
+function App() {
+  return (
+    <>
+    <VL />
+    </>
+  )
+}
+
+export default App;
---
+++ src/VL.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function VL() {
+  return (
+    <>
+    <div id="vlwrapper">
+        <div id = "navbarwrap">
+            <div id = "loginwrap">
+                <p>login</p>
+            </div>
+            <ul>
+                <li class="focusednavitem">Home</li>
+                <li class="navitems">Services</li>
+                <li class="navitems">About us</li>
+                <li class="navitems">Contact</li>
+            </ul>
+        </div>
+        <div id="homepage">
+
+        </div>
+    </div>
+    </>
+  )
+}
